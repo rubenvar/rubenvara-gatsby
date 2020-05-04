6245c215f9aceed6546fd3626a78c047bfa9adb5
Rubén <hey@rubenvara.io>
2020-05-04 13:34:36 +0200

    fix firebase connection

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,18 @@
+// ?¿?¿?¿?
+// fallaba solo la databaseURL, no sé por qué
+// https://stackoverflow.com/questions/56566333/cant-determine-firebase-database-url-when-trying-to-read-firebase-database-from
 import firebase from 'firebase/app';
 import 'firebase/database';
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 // config
 const firebaseConfig = {
   apiKey: process.env.GATSBY_FIREBASE_APIKEY,
   authDomain: process.env.GATSBY_FIREBASE_AUTHDOMAIN,
-  databaseURL: process.env.GATSBY_FIREBASE_DATABASEURL,
+  databaseURL: 'https://rvio-post-likes.firebaseio.com',
   storageBucket: process.env.GATSBY_FIREBASE_STORAGEBUCKET,
 };
 

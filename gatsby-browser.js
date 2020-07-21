ee81b05202f051fc18d0e0e48de7e64883ad6d59
Rubén <hey@rubenvara.io>
2020-07-21 13:37:29 +0200

    changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
 // import fonts here (https://github.com/KyleAMathews/typefaces)
 import 'typeface-mansalva';
 import 'typeface-martel';

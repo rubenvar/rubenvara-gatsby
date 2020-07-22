eb7ba39ff8ad3d4071db79fbebcabd2b27d48e70
Rubén <hey@rubenvara.io>
2020-07-22 21:04:58 +0200

    textx for Docs category

--- src/utils/categoryData.js
+++ src/utils/categoryData.js
@@ -1,7 +1,8 @@
 const categoryDescriptions = [
   {
     category: 'Docs',
-    description: 'Los posts con docs sobre herramientas',
+    description:
+      'Aquí están los posts con documentación sobre herramientas. Hay algunas librerías como Gulp o Pug que me encatant pero no tienen documentación en español, y he pensado que ofrecer aquí un tutorial puede ayudar a muchas personas que empiezan.',
   },
 ];
 

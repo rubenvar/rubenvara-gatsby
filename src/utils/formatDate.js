fc93698d5be182c4a50d669dc69552d0630c4d7b
Rubén <hey@rubenvara.io>
2020-11-07 01:13:22 +0100

    add function for date formatting

---
+++ src/utils/formatDate.js
@@ -0,0 +1,13 @@
+import { format } from 'date-fns';
+import { es } from 'date-fns/locale';
+
+// display year only if it's different than current year
+export function formatDate(string) {
+  const date = new Date(string);
+  const dateFormat =
+    date.getFullYear() === new Date().getFullYear()
+      ? `d 'de' MMMM`
+      : `d 'de' MMMM, yyyy`;
+
+  return format(date, dateFormat, { locale: es });
+}

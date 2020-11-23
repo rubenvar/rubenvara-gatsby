ccbf72f39a5926aa4b81b0ff12089af546a65803
Rubén <hey@rubenvara.io>
2020-11-23 17:47:14 +0100

    style Twitter box in posts

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { categoryDescriptions } from './categoryData';
 export { formatDate } from './formatDate';
+export { TwitterIcon } from './TwitterIcon';
 export { useWindowSize } from './useWindowSize';

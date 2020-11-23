8691e0854faeab1c9f15774be38c1eeb42b9e259
Rubén <hey@rubenvara.io>
2020-11-23 17:23:52 +0100

    style pagination 🎨

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,35 +2,56 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import { IconChevronLeft, IconChevronRight } from '@tabler/icons';
 
 // import StyledPaginationDefault from './styles/StyledPagination';
 
-const StyledPagination = styled.div`
-  /* ${StyledPaginationDefault};
-  margin: ${(props) => props.theme.gap120} 0 0;
+const StyledPaginationButton = styled.div`
+  height: 45px;
+  width: 100%;
   display: grid;
-  grid-template-columns: 1fr 0.6fr 1fr;
-  grid-gap: ${(props) => props.theme.gap50};
-  gap: ${(props) => props.theme.gap50};
-  font-family: 'Victor Mono', monospace;
-  div {
-    a {
-      padding: ${(props) => props.theme.gap60} ${(props) => props.theme.gap30};
-      font-family: 'Victor Mono', monospace;
-      font-weight: 700;
-    }
+  justify-items: center;
+  align-items: center;
+  ${({ disabled }) => disabled && `color: var(--grey300);`};
+  ${({ dir }) => dir === 'prev' && `border-right: 1px solid;`};
+  ${({ dir }) => dir === 'next' && `border-left: 1px solid;`};
+  border-color: var(--grey200);
+  transition: all 0.3s;
+  &:hover {
+    ${({ disabled }) => !disabled && `background-color: var(--grey100);`};
+  }
+`;
+
+const StyledPagination = styled.nav`
+  width: 32%;
+  min-width: 220px;
+  max-width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  justify-items: center;
+  align-items: center;
+  margin: 0 auto;
+  box-shadow: var(--boxShadow100);
+  border-radius: var(--smallBr);
+  a {
+    width: 100%;
   }
   p {
-    letter-spacing: -1px;
-    align-self: center;
-    text-align: center;
-    line-height: 1.35rem;
-    color: ${(props) => props.theme.grey300};
-    span {
-      color: ${(props) => props.theme.grey500};
-    }
-  } */
+    height: 45px;
+    width: 100%;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    margin: 0;
+    color: var(--grey600);
+  }
 `;
+
+const PaginationButton = ({ dir, disabled }) => (
+  <StyledPaginationButton dir={dir} disabled={disabled}>
+    {dir === 'prev' ? <IconChevronLeft /> : <IconChevronRight />}
+  </StyledPaginationButton>
+);
 
 function Pagination({
   totalCount,
@@ -41,29 +62,33 @@ function Pagination({
 
   return (
     <StyledPagination>
+      {currentPage > 1 ? (
+        <Link to={currentPage > 2 ? `/blog/${currentPage - 1}` : `/blog`}>
+          <PaginationButton dir="prev" />
+        </Link>
+      ) : (
+        <PaginationButton dir="prev" disabled />
+      )}
       <div>
-        {currentPage > 1 && (
-          <Link
-            className="prev"
-            to={currentPage > 2 ? `/blog/${currentPage - 1}` : `/blog`}
-          >
-            ←
-          </Link>
-        )}
+        <p>
+          {currentPage} de {numPages}
+        </p>
       </div>
-      <p>
-        <span>{currentPage}</span> de <span>{numPages}</span>
-      </p>
-      <div>
-        {currentPage < numPages && (
-          <Link className="next" to={`/blog/${currentPage + 1}`}>
-            →
-          </Link>
-        )}
-      </div>
+      {currentPage < numPages ? (
+        <Link to={`/blog/${currentPage + 1}`}>
+          <PaginationButton dir="next" />
+        </Link>
+      ) : (
+        <PaginationButton dir="next" disabled />
+      )}
     </StyledPagination>
   );
 }
+
+PaginationButton.propTypes = {
+  dir: PropTypes.string,
+  disabled: PropTypes.bool,
+};
 
 Pagination.propTypes = {
   totalCount: PropTypes.number.isRequired,
